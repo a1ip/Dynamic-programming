0bca4158602a57bdc8648310793b786753a66f07
Tsarev Nikita <nikts53@gmail.com>
2016-04-17 13:27:37 +0300

    Fixed extract_contester.sh once again

--- extract_contester.sh
+++ extract_contester.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 if [[ -z $1 ]]; then
     echo "Usage: $0 <contester zip>"
     exit 1
@@ -18,33 +17,37 @@ mkdir -p "$rt"
 pushd "/tmp/$pname" > /dev/null 2>&1
 
 unzip "$rt/../$zipname" > /dev/null 2>&1
-find . -type f -exec iconv -f windows-1251 -t utf-8 {} -o {} \;
-checker=$(ls ${pname}_checker* | sort -r | head -1)
-solver=$(ls ${pname}_solver* | sort -r | head -1)
-statement=$(ls ${pname}*.htm* | sort -r | head -1)
-ntests=$(ls tests/*_input.txt | wc -l)
+find . -type f -exec iconv -f windows-1251 -t utf-8 {} -o {} \; 2>/dev/null
+checker=$(ls ${pname}?checker* 2>/dev/null | sort -r | head -1)
+solver=$(ls ${pname}?solver* 2>/dev/null | sort -r | head -1)
+statement=$(ls ${pname}*.htm* 2>/dev/null | sort -r | head -1)
+ntests=$(ls tests/*_input.txt 2>/dev/null | wc -l)
 
-cp "$checker" "$rt/checker.${checker##*.}"
-cp "$solver" "$rt/solver.${solver##*.}"
-cp "$statement" "$rt/statement.${statement##*.}"
-cp -r "tests" "$rt/tests"
+cp "$checker" "$rt/checker.${checker##*.}" 2>/dev/null
+cp "$solver" "$rt/solver.${solver##*.}" 2>/dev/null
+cp "$statement" "$rt/statement.${statement##*.}" 2>/dev/null
+cp -r "tests" "$rt/tests" 2>/dev/null
 
 pushd "$rt/tests" > /dev/null 2>&1
 
-rename "${pname}_" "" *
-rename "_input.txt" ".input" *
-rename "_pattern.txt" ".pattern" *
+rename "${pname}_" "" * 2>/dev/null
+rename "_input.txt" ".input" * 2>/dev/null
+rename "_pattern.txt" ".pattern" * 2>/dev/null
+
+rename "${pname}-" "" * 2>/dev/null
+rename "-input.txt" ".input" * 2>/dev/null
+rename "-pattern.txt" ".pattern" * 2>/dev/null
 
 popd > /dev/null 2>&1
 
-timelimit=$(sed -n "s/^ *<TimeLimit platform=\"native\">\([0-9]\+\)<\/TimeLimit>/\1/p" < palindroms.xml)
-memlimit=$(sed -n "s/^ *<MemoryLimit platform=\"native\">\([0-9]\+\)<\/MemoryLimit>/\1/p" < palindroms.xml)
-problemname=$(sed -n "s/^ *<Name lang=\"ru\">\(.*\)<\/Name>/\1/p" < palindroms.xml)
+timelimit=$(sed -n "s/^ *<TimeLimit platform=\"native\">\([0-9]\+\)<\/TimeLimit>/\1/p" < ${pname}.xml)
+memlimit=$(sed -n "s/^ *<MemoryLimit platform=\"native\">\([0-9]\+\)<\/MemoryLimit>/\1/p" < ${pname}.xml)
+problemname=$(sed -n "s/^ *<Name lang=\"ru\">\(.*\)<\/Name>/\1/p" < ${pname}.xml)
 
-echo "timelimit=$timelimit" >> "$rt/meta"
-echo "memlimit=$timelimit" >> "$rt/meta"
-echo "name=$problemname" >> "$rt/meta"
-echo "ntests=$ntests" >> "$rt/meta"
+echo "timelimit=\"$timelimit\"" >> "$rt/meta"
+echo "memlimit=\"$timelimit\"" >> "$rt/meta"
+echo "name=\"$problemname\"" >> "$rt/meta"
+echo "ntests=\"$ntests\"" >> "$rt/meta"
 
 popd > /dev/null 2>&1
 
